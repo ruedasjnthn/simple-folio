eb386753e1b3d06a8f8da25ff44e546987bd4607
Jonathan  Ruedas <ruedasjnthn@gmail.com>
2023-07-01 07:06:59 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        black: "#151515",
+        "light-grey": "#242424",
+      },
+    },
+  },
+  plugins: [],
+};
