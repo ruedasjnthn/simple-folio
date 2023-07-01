1e51e397618f0870ffcab1e7df6d6c13731f4834
Jonathan  Ruedas <ruedasjnthn@gmail.com>
2023-07-01 08:55:38 +0800

    wip: layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         black: "#151515",
         "light-grey": "#242424",
+        green: "#4EE1A0",
       },
     },
   },
