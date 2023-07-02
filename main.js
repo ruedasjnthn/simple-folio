2cca0ca3a1ca24ae4a4f7f4f697b76a97f609c55
Jonathan  Ruedas <ruedasjnthn@gmail.com>
2023-07-03 05:20:28 +0800

    this commit will include additional layout and add emailjs

---
+++ main.js
@@ -0,0 +1,21 @@
+function sendEmail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_hp210wx";
+  const templateID = "template_8bnaz7m";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
